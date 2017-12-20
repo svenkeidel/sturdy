0b33b23f3150921557b017f245bdf0ae7651269c
Jente Hidskes <hjdskes@gmail.com>
2017-12-20 13:21:45 +0100

    Data.InfiniteNumbers: have InfiniteNumber derive Show
    This is required for testing with Hspec.

--- lib/src/Data/InfiniteNumbers.hs
+++ lib/src/Data/InfiniteNumbers.hs
@@ -5,7 +5,7 @@ module Data.InfiniteNumbers where
 import Data.Order
 import Data.FreeCoCompletion
 
-data InfiniteNumber a = NegInfinity | Number a | Infinity deriving (Eq,Ord)
+data InfiniteNumber a = NegInfinity | Number a | Infinity deriving (Eq,Ord,Show)
 
 isNegative :: (Eq a,Num a) => a -> Bool
 isNegative x = signum x == -1
