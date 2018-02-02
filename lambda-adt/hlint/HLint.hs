442572f440209cd84b297da2784a8384a1843c9f
Jente Hidskes <hjdskes@gmail.com>
2018-02-02 12:08:04 +0100

    Run HLint through Gitlab's CI stage
    The advantages to running HLint from CI are that
    
    1. we do not need a HLint.hs file for every project,
    2. the output of HLint is not mixed with the output of the spec tests, and
    3. linting and spec tests can pass/fail/be cancelled independently.

--- lambda-adt/hlint/HLint.hs
+++
@@ -1,9 +0,0 @@
-module Main(main) where
-
-import Language.Haskell.HLint3 (hlint)
-import System.Exit (exitFailure, exitSuccess)
-
-main :: IO ()
-main = do
-    hints <- hlint ["src"]
-    if null hints then exitSuccess else exitFailure
