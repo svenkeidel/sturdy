2430115eaef03720050a886ff256dbfcb9c5215f
Sven Keidel <svenkeidel@gmail.com>
2018-02-08 10:25:18 +0100

    add test for stateful recursive program

--- lib/test/FixpointCacheSpec.hs
+++ lib/test/FixpointCacheSpec.hs
@@ -12,6 +12,7 @@ import           Data.Function
 import           Control.Arrow
 import           Control.Arrow.Fix
 import           Control.Arrow.Fail
+import           Control.Arrow.State
 
 import           Data.Interval (Interval)
 import           Data.Sign (Sign)
@@ -26,39 +27,52 @@ main = hspec spec
 
 type Cache x y = CacheArrow x y x y
 type ErrorCache x y = ErrorArrow () (CacheArrow x (Error () y)) x y
+type StateCache x y = StateArrow Int (CacheArrow (Int,x) (Int,y)) x y
 
 spec :: Spec
 spec = do
   describe "the analysis of the fibonacci numbers" $
-    it "should memoize numbers that have been computed before already" $ do
-      runCacheArrow (fixA fib :: Cache Int (Interval Int)) 10 `shouldBe` fix fib 10
-      runCacheArrow (fixA fib :: Cache Int (Interval Int)) 15 `shouldBe` fix fib 15
+    let fib :: ArrowChoice c => c Int (Interval Int) -> c Int (Interval Int)
+        fib f = proc n -> case n of
+          0 -> returnA -< 0
+          1 -> returnA -< 1
+          _ -> do
+            x <- f -< n-1
+            y <- f -< n-2
+            returnA -< x + y
+    in it "should memoize numbers that have been computed before already" $ do
+         runCacheArrow (fixA fib :: Cache Int (Interval Int)) 10 `shouldBe` fix fib 10
+         runCacheArrow (fixA fib :: Cache Int (Interval Int)) 15 `shouldBe` fix fib 15
 
   describe "the analyis of a diverging program" $
-    it "should terminate with bottom" $
-      runCacheArrow (fixA diverge :: Cache Int Sign) 5 `shouldBe` bottom
+    let diverge :: Cache Int Sign -> Cache Int Sign
+        diverge f = proc n -> case n of
+          0 -> f -< 0
+          _ -> f -< (n-1)
+    in it "should terminate with bottom" $
+         runCacheArrow (fixA diverge) 5
+           `shouldBe` bottom
 
   describe "the analysis of a failing program" $
-    it "should fail, but update the fixpoint cache" $
-      runCacheArrow' (runErrorArrow (fixA recurseFail :: ErrorCache Int Sign)) 5 `shouldBe` (S.fromList [(n,Error ()) | n <- [0..4]], Error ())
+    let recurseFail :: ErrorCache Int Sign -> ErrorCache Int Sign
+        recurseFail f = proc n -> case n of
+          0 -> failA -< ()
+          _ -> f -< (n-1)
+    in it "should fail, but update the fixpoint cache" $
+         runCacheArrow' (runErrorArrow (fixA recurseFail)) 5
+            `shouldBe` (S.fromList [(n,Error ()) | n <- [0..4]], Error ())
 
-  where
-    fib :: ArrowChoice c => c Int (Interval Int) -> c Int (Interval Int)
-    fib f = proc n -> case n of
-      0 -> returnA -< 0
-      1 -> returnA -< 1
-      _ -> do
-        x <- f -< n-1
-        y <- f -< n-2
-        returnA -< x + y
+  describe "the analysis of a stateful program" $
+    let timesTwo :: StateCache Int () -> StateCache Int ()
+        timesTwo f = proc n -> case n of
+          0 -> returnA -< ()
+          _ -> do
+            s <- getA -< ()
+            putA -< s+1
+            f -< (n-1)
+            s' <- getA -< ()
+            putA -< s'+1
 
-    diverge :: ArrowChoice c => c Int Sign -> c Int Sign
-    diverge f = proc n -> case n of
-      0 -> f -< 0
-      _ -> f -< (n-1)
-
-    recurseFail :: (ArrowFail () c, ArrowChoice c) => c Int Sign -> c Int Sign
-    recurseFail f = proc n -> case n of
-      0 -> failA -< ()
-      _ -> f -< (n-1)
-
+    in it "should cache the state of the program" $
+         runCacheArrow' (runStateArrow (fixA timesTwo)) (0,5)
+           `shouldBe` (S.fromList [((n,5-n),(10-n,())) | n <- [1..5]],(10,()))
