1b05b0b7797ca7cac664869c02421d81dafcad6c
Matthijs Bijman <matthijs@bijman.org>
2018-05-04 10:29:38 +0200

    Feature parity between arrow and simple interpreters

---
+++ lambdajs/src/ConcreteArrow.hs
@@ -0,0 +1,206 @@
+{-# LANGUAGE Arrows #-}
+module ConcreteArrow where
+
+import Syntax
+
+import Control.Category
+import Control.Arrow
+
+import Data.Fixed (mod')
+import Data.List (isPrefixOf)
+import Data.Bits (shift, bit)
+import Data.Word (Word32)
+
+import Data.Map (Map, findWithDefault)
+import qualified Data.Map as Map
+
+type Store = Map String Value
+lookup :: String -> Store -> Maybe Value
+lookup s st = Map.lookup s st 
+empty :: Store
+empty = Map.empty
+insert :: String -> Value -> Store -> Store
+insert s v st = Map.insert s v st
+
+
+newtype ConcreteArr x y = ConcreteArr {runArr :: x -> Store -> Either String (Store, y) } 
+
+instance Category ConcreteArr where
+    id = ConcreteArr (\x st -> Right (st, x))
+    ConcreteArr f . ConcreteArr g = ConcreteArr $ \x st -> case g x st of
+        Left er -> Left er
+        Right (st', y) -> f y st'
+
+instance Arrow ConcreteArr where
+    arr f = ConcreteArr (\x st -> Right (st, f x))
+    first (ConcreteArr f) = ConcreteArr $ \(x, y) st -> case f x st of
+        Left er -> Left er
+        Right (st', z) -> Right (st', (z, y))
+
+instance ArrowChoice ConcreteArr where
+    left (ConcreteArr f) = ConcreteArr $ \e st -> case e of
+        Left x -> case f x st of
+            Left err -> Left err
+            Right (st', y) -> Right (st', Left y)
+        Right z -> Right (st, Right z)
+
+-- Constructs an arrow that operates on a list of inputs producing a list of outputs.
+-- Used for evaluating a list of arguments, producing a list of values which are passed to the evalOp function.
+mapA :: ArrowChoice arr => arr a b -> arr [a] [b]
+mapA f = arr (\list -> case list of 
+    [] -> Left ()
+    (x : xs) -> Right (x, xs)) >>> (arr (const []) ||| ((f *** mapA f) >>> (arr (uncurry (:)))))
+
+get :: ConcreteArr () Store
+get = ConcreteArr (\() st -> Right (st, st))
+
+put :: ConcreteArr Store ()
+put = ConcreteArr (\st _ -> Right (st, ()))
+
+throw :: ConcreteArr String a
+throw = ConcreteArr (\er _ -> Left er)
+
+evalOp :: Op -> [Value] -> Value
+-- number operators
+evalOp ONumPlus [(VNumber a), (VNumber b)] =
+    VNumber (a + b)
+evalOp OMul [(VNumber a), (VNumber b)] =
+    VNumber (a * b)
+evalOp ODiv [(VNumber a), (VNumber b)] =
+    VNumber (a / b)
+evalOp OMod [(VNumber a), (VNumber b)] =
+    VNumber (mod' a b)
+evalOp OSub [(VNumber a), (VNumber b)] =
+    VNumber (a - b)
+evalOp OLt [(VNumber a), (VNumber b)] =
+    VBool (a < b)
+evalOp OToInteger [(VNumber a)] = VNumber $ fromInteger (truncate a)
+evalOp OToInt32 [(VNumber a)] = 
+    let n = mod (truncate a) ((2^32) :: Integer) in
+        if n > (2^31) then VNumber $ fromInteger $ n - (2^32)
+        else VNumber $ fromInteger $ n
+evalOp OToUInt32 [(VNumber a)] = VNumber $ fromInteger $ mod (abs $ truncate a) (2^32)
+
+-- shift operators
+evalOp OLShift [(VNumber a), (VNumber b)] = 
+    VNumber $ fromInteger $ shift (truncate a) (truncate b)
+evalOp OSpRShift [(VNumber a), (VNumber b)] =
+    VNumber $ fromInteger $ shift (truncate a) (- (truncate b))
+evalOp OZfRShift [(VNumber a), (VNumber b)] =
+    VNumber $ fromInteger $ shift (fromIntegral $ (truncate a :: Word32)) (- (truncate b))
+
+-- string operators
+evalOp OStrPlus [(VString a), (VString b)] = 
+    VString (a ++ b)
+evalOp OStrLt [(VString a), (VString b)] =
+    VBool (a < b)
+evalOp OStrLen [(VString a)] =
+    VNumber $ fromIntegral $ length a
+evalOp OStrStartsWith [(VString a), (VString b)] =
+    VBool $ isPrefixOf b a
+
+-- boolean operators
+evalOp OBAnd [(VBool a), (VBool b)] = 
+    VBool (a && b)
+evalOp OBOr [(VBool a), (VBool b)] = 
+    VBool (a || b)
+evalOp OBXOr [(VBool a), (VBool b)] = 
+    VBool (a /= b)
+evalOp OBNot [(VBool a)] = 
+    VBool (not a)
+
+-- isPrimitive operator
+evalOp OIsPrim [(VNumber _)]  = VBool True
+evalOp OIsPrim [(VString _)]  = VBool True
+evalOp OIsPrim [(VBool _)]    = VBool True
+evalOp OIsPrim [(VNull)]      = VBool True
+evalOp OIsPrim [(VUndefined)] = VBool True
+evalOp OIsPrim [(VObject _)]  = VBool False
+
+-- primitive to number operator
+evalOp OPrimToNum [(VNumber a)] = VNumber a
+evalOp OPrimToNum [(VString st)] = VNumber (read st :: Double)
+evalOp OPrimToNum [(VBool b)] = if b then VNumber 1.0 else VNumber 0.0
+evalOp OPrimToNum [(VNull)] = VNumber 0
+evalOp OPrimToNum [(VUndefined)] = VNumber (0/0)
+-- #todo object conversions -> valueOf call
+
+-- primitive to string operator
+evalOp OPrimToStr [(VNumber a)]  = VString $ show a
+evalOp OPrimToStr [(VString st)] = VString st
+evalOp OPrimToStr [(VBool b)]    = VString $ show b
+evalOp OPrimToStr [(VNull)]      = VString "null"
+evalOp OPrimToStr [(VUndefined)] = VString "undefined"
+evalOp OPrimToStr [(VObject _)]  = VString "object"
+
+-- primitive to bool operator
+evalOp OPrimToBool [(VNumber a)]  = VBool $ (a /= 0.0) && (not (isNaN a))
+evalOp OPrimToBool [(VString st)] = VBool $ not $ st == ""
+evalOp OPrimToBool [(VBool b)]    = VBool b
+evalOp OPrimToBool [(VNull)]      = VBool False
+evalOp OPrimToBool [(VUndefined)] = VBool False
+evalOp OPrimToBool [(VObject _)]  = VBool True
+
+-- typeOf operator
+evalOp OTypeof [(VNumber _)]   = VString "number"
+evalOp OTypeof [(VString _)]   = VString "string"
+evalOp OTypeof [(VBool _)]     = VString "boolean"
+evalOp OTypeof [(VUndefined)]  = VString "undefined"
+evalOp OTypeof [(VNull)]       = VString "object"
+evalOp OTypeof [(VLambda _ _)] = VString "function"
+evalOp OTypeof [(VObject _)]   = VString "object"
+
+-- equality operators
+evalOp OStrictEq [a, b] = VBool $ a == b
+evalOp OAbstractEq [(VNumber a), (VNumber b)] = VBool $ a == b
+evalOp OAbstractEq [(VNull), (VUndefined)] = VBool True
+evalOp OAbstractEq [(VUndefined), (VNull)] = VBool True
+evalOp OAbstractEq [(VNumber a), (VString b)] = 
+    VBool $ (VNumber a) == (evalOp OPrimToNum [(VString b)])
+evalOp OAbstractEq [(VString a), (VNumber b)] =
+    VBool $ (evalOp OPrimToNum [(VString a)]) == (VNumber b)
+evalOp OAbstractEq [(VBool a), (VNumber b)] =
+    VBool $ (evalOp OPrimToNum [(VBool a)]) == (VNumber b)
+evalOp OAbstractEq [(VNumber a), (VBool b)] =
+    VBool $ (VNumber a) == (evalOp OPrimToNum [(VBool b)])
+
+-- math operators
+evalOp OMathExp [(VNumber a)] = VNumber $ exp a
+evalOp OMathLog [(VNumber a)] = VNumber $ log a
+evalOp OMathCos [(VNumber a)] = VNumber $ cos a
+evalOp OMathSin [(VNumber a)] = VNumber $ sin a
+evalOp OMathAbs [(VNumber a)] = VNumber $ abs a
+evalOp OMathPow [(VNumber a), (VNumber b)] = VNumber $ a ** b
+
+-- object operators
+evalOp OHasOwnProp [(VObject fields), (VString field)] =
+    VBool $ any (\(name, value) -> (name == field)) fields
+
+eval :: ConcreteArr Expr Value
+eval = proc e -> case e of
+    ENumber d -> returnA -< VNumber d
+    EString s -> returnA -< VString s
+    EBool b -> returnA -< VBool b
+    EUndefined -> returnA -< VUndefined
+    ENull -> returnA -< VNull
+    ELambda ids exp -> returnA -< VLambda ids exp
+    EObject fields -> do
+        vals <- (mapA $ second eval) -< fields
+        returnA -< VObject vals
+    EId id -> do
+        st <- get -< ()
+        case Map.lookup id st of
+            Just v -> returnA -< v
+            Nothing -> returnA -< VUndefined
+    EOp op exps -> do
+        vals <- mapA eval -< exps
+        returnA -< evalOp op vals
+    EApp (ELambda params body) args -> do
+        vals <- mapA eval -< args
+        zipped <- arr $ uncurry zip -< (params, vals)
+
+        scope <- get -< ()
+        put -< Map.fromList $ (Map.toList scope) ++ (zipped)
+
+        res <- eval -< body 
+        returnA -< res
--- lambdajs/test/ConcreteSpec.hs
+++ lambdajs/test/ConcreteSpec.hs
@@ -1,149 +1,156 @@
 module ConcreteSpec where
 
 import Syntax
-import Concrete
+import ConcreteArrow (Store, ConcreteArr, runArr, lookup, empty, insert)
+import qualified ConcreteArrow as CA (eval)
 
 import Test.Hspec
+
 import Data.Fixed (mod')
 
 main :: IO ()
 main = hspec spec
+
+eval :: Store -> Expr -> Either String Value
+eval st e = case runArr (CA.eval) e st of
+  Left err -> Left err
+  Right (_ , v) -> Right v
 
 spec :: Spec
 spec = do
   describe "literals" $ do
     it "number literals" $ do
       let program = ENumber 2.0
-      eval scope program `shouldBe` VNumber 2.0
+      eval scope program `shouldBe` Right (VNumber 2.0)
     it "boolean literals" $ do
       let program = EBool True
-      eval scope program `shouldBe` VBool True
+      eval scope program `shouldBe` Right (VBool True)
     it "string literals" $ do
       let program = EString "test"
-      eval scope  program `shouldBe` VString "test"
+      eval scope  program `shouldBe` Right (VString "test")
     it "undefined literal" $ do
-      eval scope EUndefined `shouldBe` VUndefined
+      eval scope EUndefined `shouldBe` Right (VUndefined)
     it "null literal" $ do
-      eval scope ENull `shouldBe` VNull
+      eval scope ENull `shouldBe` Right (VNull)
     it "lambda literal" $ do
-      eval scope (ELambda [] (ENumber 1.0)) `shouldBe` (VLambda [] (ENumber 1.0))
+      eval scope (ELambda [] (ENumber 1.0)) `shouldBe` Right ((VLambda [] (ENumber 1.0)))
   
   describe "objects" $ do
     it "object with numbers" $ do
       let program = EObject [("a", ENumber 2.0), ("b", ENumber 3.0)]
-      eval scope program `shouldBe` VObject [("a", VNumber 2.0), ("b", VNumber 3.0)]
+      eval scope program `shouldBe` Right (VObject [("a", VNumber 2.0), ("b", VNumber 3.0)])
   
   describe "identifiers" $ do
     it "single identifier" $ do 
       let program = EId "a"
-      let scopeWithId = Scope (insert "a" (VNumber 1.0) (values scope)) (parent scope)
-      eval scopeWithId program `shouldBe` VNumber 1.0
+      let scopeWithId = insert "a" (VNumber 1.0) empty
+      eval scopeWithId program `shouldBe` Right (VNumber 1.0)
     
   describe "operators" $ do
     -- numer operators
     describe "numbers" $ do
       it "addition" $ do
         let program = EOp ONumPlus [(ENumber 1.0), (ENumber 2.0)]
-        eval scope program `shouldBe` VNumber 3.0
+        eval scope program `shouldBe` Right (VNumber 3.0)
       it "multiplication" $ do
         let program = EOp OMul [(ENumber 3.0), (ENumber 2.0)]
-        eval scope program `shouldBe` VNumber 6.0
+        eval scope program `shouldBe` Right (VNumber 6.0)
       it "division" $ do
         let program = EOp ODiv [(ENumber 3.0), (ENumber 2.0)]
-        eval scope program `shouldBe` VNumber 1.5
+        eval scope program `shouldBe` Right (VNumber 1.5)
       it "modulus" $ do
         let program = EOp OMod [(ENumber 3.0), (ENumber 0.7)]
-        eval scope program `shouldBe` VNumber (mod' (3.0 :: Double) (0.7 :: Double))
+        eval scope program `shouldBe` Right (VNumber (mod' (3.0 :: Double) (0.7 :: Double)))
       it "subtraction" $ do
         let program = EOp OSub [(ENumber 3.0), (ENumber 2.0)]
-        eval scope program `shouldBe` VNumber 1.0
+        eval scope program `shouldBe` Right (VNumber 1.0)
       it "less than" $ do
         let program = EOp OLt [(ENumber 3.0), (ENumber 4.0)]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "to integer" $ do
         let program = EOp OToInteger [(ENumber 3.5)]
-        eval scope program `shouldBe` VNumber 3.0
+        eval scope program `shouldBe` Right (VNumber 3.0)
       it "to int32" $ do
         let program = EOp OToInt32 [(ENumber ((2^31) + 1))]
-        eval scope program `shouldBe` VNumber (-(2^31) + 1)
+        eval scope program `shouldBe` Right (VNumber (-(2^31) + 1))
       it "to uint32" $ do
         let program = EOp OToUInt32 [(ENumber (-(2^31)))]
-        eval scope program `shouldBe` VNumber (2^31)
+        eval scope program `shouldBe` Right (VNumber (2^31))
 
     -- string operators
     describe "strings" $ do
       it "addition" $ do
         let program = EOp OStrPlus [(EString "a"), (EString "b")]
-        eval scope program `shouldBe` VString "ab"
+        eval scope program `shouldBe` Right (VString "ab")
       it "less than" $ do
         let program = EOp OStrLt [(EString "a"), (EString "b")]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "length" $ do
         let program = EOp OStrLen [(EString "abc")]
-        eval scope program `shouldBe` VNumber 3.0
+        eval scope program `shouldBe` Right (VNumber 3.0)
       it "starts with" $ do
         let program = EOp OStrStartsWith [(EString "abc"), (EString "a")]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
 
     -- bool operators
     describe "bool" $ do
       it "and" $ do
         let program = EOp OBAnd [(EBool True), (EBool False)]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
       it "or" $ do
         let program = EOp OBOr [(EBool True), (EBool False)]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "xor" $ do
         let program = EOp OBXOr [(EBool True), (EBool False)]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "not" $ do
         let program = EOp OBNot [(EBool True)]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
 
     -- typeof
     describe "typeof" $ do
       it "number" $ do
         let program = EOp OTypeof [(ENumber 1.0)]
-        eval scope program `shouldBe` VString "number"
+        eval scope program `shouldBe` Right (VString "number")
       it "string" $ do
         let program = EOp OTypeof [(EString "a")]
-        eval scope program `shouldBe` VString "string"
+        eval scope program `shouldBe` Right (VString "string")
       it "bool" $ do
         let program = EOp OTypeof [(EBool True)]
-        eval scope program `shouldBe` VString "boolean"
+        eval scope program `shouldBe` Right (VString "boolean")
       it "undefined" $ do
         let program = EOp OTypeof [(EUndefined)]
-        eval scope program `shouldBe` VString "undefined" 
+        eval scope program `shouldBe` Right (VString "undefined" )
       it "function" $ do
         let program = EOp OTypeof [(ELambda [] (ENumber 1.0))]
-        eval scope program `shouldBe` VString "function"  
+        eval scope program `shouldBe` Right (VString "function"  )
       it "object" $ do
         let program = EOp OTypeof [(EObject [])]
-        eval scope program `shouldBe` VString "object"
+        eval scope program `shouldBe` Right (VString "object")
   
     -- is primitive
     describe "is primitive" $ do
       it "number" $ do
         let program = EOp OIsPrim [(ENumber 1.0)]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "string" $ do
         let program = EOp OIsPrim [(EString "a")]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "object" $ do
         let program = EOp OIsPrim [(EObject [])]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
 
     -- primitive to number
     describe "to number conversions" $ do
       it "number" $ do
         let program = EOp OPrimToNum [(ENumber 1.0)]
-        eval scope program `shouldBe` VNumber 1.0
+        eval scope program `shouldBe` Right (VNumber 1.0)
       it "string" $ do
         let program = EOp OPrimToNum [(EString "1.0")]
-        eval scope program `shouldBe` VNumber 1.0
+        eval scope program `shouldBe` Right (VNumber 1.0)
       it "null" $ do
         let program = EOp OPrimToNum [(ENull)]
-        eval scope program `shouldBe` VNumber 0.0
+        eval scope program `shouldBe` Right (VNumber 0.0)
       it "undefined" $ do
         let program = EOp OPrimToNum [(EUndefined)]
         eval scope program `shouldSatisfy` (\a -> a /= a)
@@ -152,134 +159,134 @@ spec = do
     describe "to string conversions" $ do
       it "number" $ do
         let program = EOp OPrimToStr [(ENumber 1.0)]
-        eval scope program `shouldBe` VString "1.0"
+        eval scope program `shouldBe` Right (VString "1.0")
       it "string" $ do
         let program = EOp OPrimToStr [(EString "1.0")]
-        eval scope program `shouldBe` VString "1.0"
+        eval scope program `shouldBe` Right (VString "1.0")
       it "null" $ do
         let program = EOp OPrimToStr [(ENull)]
-        eval scope program `shouldBe` VString "null"
+        eval scope program `shouldBe` Right (VString "null")
       it "undefined" $ do
         let program = EOp OPrimToStr [(EUndefined)]
-        eval scope program `shouldBe` VString "undefined"
+        eval scope program `shouldBe` Right (VString "undefined")
       it "object" $ do
         let program = EOp OPrimToStr [(EObject [])]
-        eval scope program `shouldBe` VString "object"
+        eval scope program `shouldBe` Right (VString "object")
 
     -- primitive to bool
     describe "to bool conversions" $ do
       it "number" $ do
         let program = EOp OPrimToBool [(ENumber 1.0)]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "nan" $ do
         let program = EOp OPrimToBool [(ENumber $ 0.0/0.0)]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
       it "zero" $ do
         let program = EOp OPrimToBool [(ENumber 0.0)]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
       it "string" $ do
         let program = EOp OPrimToBool [(EString "1.0")]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "empty string" $ do
         let program = EOp OPrimToBool [(EString "")]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
       it "null" $ do
         let program = EOp OPrimToBool [(ENull)]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
       it "undefined" $ do
         let program = EOp OPrimToBool [(EUndefined)]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
       it "object" $ do
         let program = EOp OPrimToBool [(EObject [])]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
 
     -- shifts
     describe "shifts" $ do
       it "left shift" $ do
         let program = EOp OLShift [(ENumber 2.0), (ENumber 2.0)]
-        eval scope program `shouldBe` VNumber 8.0
+        eval scope program `shouldBe` Right (VNumber 8.0)
       it "neg sign preserving right shift" $ do
         let program = EOp OSpRShift [(ENumber (-2.0)), (ENumber 3.0)]
-        eval scope program `shouldBe` VNumber (-1.0)
+        eval scope program `shouldBe` Right (VNumber (-1.0))
       it "sign preserving right shift" $ do
         let program = EOp OSpRShift [(ENumber 8.0), (ENumber 2.0)]
-        eval scope program `shouldBe` VNumber 2.0
+        eval scope program `shouldBe` Right (VNumber 2.0)
       it "zero filling right shift" $ do
         let program = EOp OZfRShift [(ENumber (-2.0)), (ENumber 3.0)]
-        eval scope program `shouldBe` VNumber 536870911
+        eval scope program `shouldBe` Right (VNumber 536870911)
     
     -- strict equality
     describe "strict equality" $ do
       it "number" $ do
         let program = EOp OStrictEq [(ENumber 1.0), (ENumber 1.0)]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "number string" $ do
         let program = EOp OStrictEq [(ENumber 1.0), (EString "1.0")]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
       it "number nan" $ do
         let program = EOp OStrictEq [(ENumber (0.0/0.0)), (ENumber (0.0/0.0))]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
 
     -- abstract equality
     describe "abstract equality" $ do
       it "number" $ do
         let program = EOp OAbstractEq [(ENumber 1.0), (ENumber 1.0)]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "number nan" $ do
         let program = EOp OAbstractEq [(ENumber (0.0/0.0)), (ENumber (0.0/0.0))]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
       it "number string" $ do
         let program = EOp OAbstractEq [(ENumber 1.0), (EString "1.0")]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "undefined null" $ do
         let program = EOp OAbstractEq [(ENull), (EUndefined)]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "bool number" $ do
         let program = EOp OAbstractEq [(EBool True), (ENumber 1.0)]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "bool number false" $ do
         let program = EOp OAbstractEq [(EBool True), (ENumber 0.0)]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
 
     -- math functions
     describe "math functions" $ do
       it "exp" $ do
         let program = EOp OMathExp [(ENumber 3.5)]
-        eval scope program `shouldBe` VNumber 33.11545195869231
+        eval scope program `shouldBe` Right (VNumber 33.11545195869231)
       it "log" $ do
         let program = EOp OMathLog [(ENumber 3.5)]
-        eval scope program `shouldBe` VNumber 1.252762968495368
+        eval scope program `shouldBe` Right (VNumber 1.252762968495368)
       it "cos" $ do
         let program = EOp OMathCos [(ENumber 3.5)]
-        eval scope program `shouldBe` VNumber (-0.9364566872907963)
+        eval scope program `shouldBe` Right (VNumber (-0.9364566872907963))
       it "sin" $ do
         let program = EOp OMathSin [(ENumber 3.5)]
-        eval scope program `shouldBe` VNumber (-0.35078322768961984)
+        eval scope program `shouldBe` Right (VNumber (-0.35078322768961984))
       it "abs pos" $ do
         let program = EOp OMathAbs [(ENumber 3.5)]
-        eval scope program `shouldBe` VNumber 3.5
+        eval scope program `shouldBe` Right (VNumber 3.5)
       it "abs neg" $ do
         let program = EOp OMathAbs [(ENumber (-3.5))]
-        eval scope program `shouldBe` VNumber 3.5
+        eval scope program `shouldBe` Right (VNumber 3.5)
       it "pow" $ do
         let program = EOp OMathPow [(ENumber 3.5), (ENumber 4.2)]
-        eval scope program `shouldBe` VNumber 192.79056951583615
+        eval scope program `shouldBe` Right (VNumber 192.79056951583615)
 
     -- object functions
     describe "object functions" $ do
       it "has own prop" $ do
         let program = EOp OHasOwnProp [(EObject [("a", ENumber 1.0)]), (EString "a")]
-        eval scope program `shouldBe` VBool True
+        eval scope program `shouldBe` Right (VBool True)
       it "has not own prop" $ do
         let program = EOp OHasOwnProp [(EObject [("a", ENumber 1.0)]), (EString "b")]
-        eval scope program `shouldBe` VBool False
+        eval scope program `shouldBe` Right (VBool False)
     
     -- lambda application
     describe "lambda application" $ do
       it "identity" $ do
         let program = EApp (ELambda ["a"] (EId "a")) [(ENumber 1.0)]
-        eval scope program `shouldBe` VNumber 1.0
+        eval scope program `shouldBe` Right (VNumber 1.0)
       it "double" $ do
         let program = EApp (ELambda ["a"] (EOp ONumPlus [(EId "a"), (EId "a")])) [(ENumber 1.0)]
-        eval scope program `shouldBe` VNumber 2.0
-  where scope = emptyScope
+        eval scope program `shouldBe` Right (VNumber 2.0)
+  where scope = empty
