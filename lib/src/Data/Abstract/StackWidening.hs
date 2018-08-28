bac3737ee30e73ea47b33edf1d36ffa79a4ea463
Sven Keidel <svenkeidel@gmail.com>
2018-08-28 10:02:28 +0200

    implement stack widening operators for stratego

--- lib/src/Data/Abstract/StackWidening.hs
+++ lib/src/Data/Abstract/StackWidening.hs
@@ -8,8 +8,15 @@
 {-# LANGUAGE RankNTypes #-}
 module Data.Abstract.StackWidening where
 
-import Control.Monad.State
-import Data.Order
+import           Control.Monad.State
+
+import           Data.Order
+import           Data.HashMap.Lazy(HashMap)
+import qualified Data.HashMap.Lazy as M
+import           Data.Identifiable
+import           Data.Monoidal
+import           Data.Maybe
+import           Data.Abstract.Widening(Widening)
 
 -- | A stack widening operator (▽ :: s -> a -> (s,a)) follows the same
 -- idea as a regular widening operator, but does not have the
@@ -24,7 +31,6 @@ import Data.Order
 -- such that x1', x2', x3' ... repeats itself, i.e.  there exists n,m
 -- with n /= m and xn' = xm'.
 type StackWidening s a = a -> State (s a) a
-type StackWidening' a = a -> a
 
 data Unit a = Unit
 instance Monoid (Unit a) where
@@ -40,27 +46,55 @@ instance Monoid (Stack a) where
   mempty = Stack 0 []
   mappend (Stack n st) (Stack n' st') = Stack (n+n') (st ++ st')
 
-stack :: (Stack a -> StackWidening' a) -> StackWidening Stack a
-stack f x = state $ \s@(Stack n st) -> let x' = f s x in (x',Stack (n+1) (x':st))
+-- | Pushes elements onto a stack and increases its size. Always calls
+-- the given widening.
+stack :: StackWidening Stack a -> StackWidening Stack a
+stack f x = state $ \s@(Stack n st) -> let x' = evalState (f x) s in (x',Stack (n+1) (x':st))
 
 -- | Return the same elements until the specified maximum stack size
 -- is reached, then call the fallback widening.
-maxSize :: Int -> StackWidening' a -> (Stack a -> StackWidening' a)
-maxSize limit fallback (Stack n _) x =
+maxSize :: Int -> StackWidening Stack a -> StackWidening Stack a
+maxSize limit fallback x = do
+  Stack n _ <- get
   if n <= limit
-  then x
+  then return x
   else fallback x
 
 -- | Reuse an element from the stack that is greater than the current
 -- element. If no such elements exist, call the fallback widening.
-reuse :: (Show a, PreOrd a) => (a -> [a] -> a) -> StackWidening' a -> (Stack a -> StackWidening' a)
-reuse bestChoice fallback (Stack _ st) x = do
+reuse :: (Show a, PreOrd a) => (a -> [a] -> a) -> StackWidening Stack a -> StackWidening Stack a
+reuse bestChoice fallback x = do
+  Stack _ st <- get
   -- All elements in the stack that are greater than the current
   -- element are potential candidates.
   let candidates = [ y | y <- st, x ⊑ y ]
-  if | null st               -> x
-     | not (null candidates) -> bestChoice x candidates
+  if | null st               -> return x
+     | not (null candidates) -> return $ bestChoice x candidates
      | otherwise             -> fallback x
+
+data Categories k b s a = Categories (HashMap k (s b))
+instance (Identifiable k, Monoid (s b)) => Monoid (Categories k b s a) where
+  mempty = Categories M.empty
+  mappend (Categories m) (Categories m') = Categories (m `mappend` m')
+
+categorize :: (Monoid (s b), Identifiable k) => Iso a (k,b) -> StackWidening s b -> StackWidening (Categories k b s) a
+categorize iso w a = do
+  Categories m <- get
+  let (k,b) = to iso a
+      s = fromMaybe mempty (M.lookup k m)
+      (b',s') = runState (w b) s
+  put $ Categories $ M.insert k s' m
+  return (from iso (k,b'))
+
+fromWidening :: Complete a => Widening a -> StackWidening Maybe a
+fromWidening w a = do
+  m <- get
+  case m of
+    Nothing -> do put (Just a); return a
+    Just x -> do
+      let x' = a `w` (x ⊔ a)
+      put (Just x')
+      return x'
 
 data Product s1 s2 x where
   Product :: s1 a -> s2 b -> Product s1 s2 (a,b)
@@ -73,5 +107,5 @@ data Product s1 s2 x where
   put $ Product s1' s2'
   return (a',b')
 
-topOut :: UpperBounded a => StackWidening' a
-topOut _ = top
+topOut :: UpperBounded a => StackWidening s a
+topOut _ = return top
